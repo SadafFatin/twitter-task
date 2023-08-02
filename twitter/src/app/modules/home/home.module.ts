729708bd1cb887c6fa58e5868c80c8ecc7b827d3
Sadaf Fatin <shoumick922@gmail.com>
2023-08-02 16:24:37 +0600

    Initial commit

---
+++ twitter/src/app/modules/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../../components/home/home.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule,
+
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
